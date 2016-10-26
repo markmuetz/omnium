bd814539ed15b708808e5a04153e10acb0822e05
Mark Muetzelfeldt <markmuetz@gmail.com>
2016-10-26 16:09:03 +0100

    Keep omnium viewer the same when viewing UM files.

--- omnium/experimental/omnium_viewer.py
+++ omnium/experimental/omnium_viewer.py
@@ -358,20 +358,36 @@ class MainWindow(QtGui.QMainWindow):
         # Add a grid.
         grid = gl.GLGridItem()
         # TODO: these need to be calculated properly.
-        grid.setSize(96, 96, 0)
-        grid.setSpacing(4, 4, 0)
+        if self.data_source == 'MONC':
+            grid.setSize(96, 96, 0)
+            grid.setSpacing(4, 4, 0)
 
-        zgrid = gl.GLGridItem()
-        zgrid.setSize(96, 48, 0)
-        zgrid.rotate(90, 1, 0, 0)
-        zgrid.translate(0, -48, 24)
-        zgrid.setSpacing(4, 4, 0)
+            zgrid = gl.GLGridItem()
+            zgrid.setSize(96, 48, 0)
+            zgrid.rotate(90, 1, 0, 0)
+            zgrid.translate(0, -48, 24)
+            zgrid.setSpacing(4, 4, 0)
 
-        zgrid2 = gl.GLGridItem()
-        zgrid2.setSize(48, 96, 0)
-        zgrid2.rotate(90, 0, 1, 0)
-        zgrid2.translate(-48, 0, 24)
-        zgrid2.setSpacing(4, 4, 0)
+            zgrid2 = gl.GLGridItem()
+            zgrid2.setSize(48, 96, 0)
+            zgrid2.rotate(90, 0, 1, 0)
+            zgrid2.translate(-48, 0, 24)
+            zgrid2.setSpacing(4, 4, 0)
+        else:
+            grid.setSize(64, 64, 0)
+            grid.setSpacing(4, 4, 0)
+
+            zgrid = gl.GLGridItem()
+            zgrid.setSize(64, 32, 0)
+            zgrid.rotate(90, 1, 0, 0)
+            zgrid.translate(0, -32, 16)
+            zgrid.setSpacing(4, 4, 0)
+
+            zgrid2 = gl.GLGridItem()
+            zgrid2.setSize(32, 64, 0)
+            zgrid2.rotate(90, 0, 1, 0)
+            zgrid2.translate(-32, 0, 16)
+            zgrid2.setSpacing(4, 4, 0)
 
         self.view.addItem(grid)
         self.view.addItem(zgrid)
