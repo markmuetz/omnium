0bdcbb60b4be7b04def5bb87e3b1991d95efdf44
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-05-31 09:17:16 +0100

    Make fetch a little more chatty.

--- omnium/cmds/fetch.py
+++ omnium/cmds/fetch.py
@@ -1,8 +1,11 @@
 import os
 from glob import glob
+from logging import getLogger
 
 from omnium.syncher import Syncher
 from omnium.suite import Suite
+
+logger = getLogger('omnium')
 
 ARGS = [(['filenames'], {'help': 'Filenames to fetch', 'nargs': '+'}),
         (['--remote', '-r'], {'help': 'Remote'}),
@@ -19,4 +22,9 @@ def main(args):
     rel_filenames = []
     rel_dir = os.path.relpath(os.getcwd(), suite.suite_dir)
     rel_filenames = [os.path.join(rel_dir, fn) for fn in sorted(filenames)]
+    logger.debug('fetch from: {} ({}:{})'.format(syncher.remote_name, 
+                                                 syncher.remote_host,
+                                                 os.path.join(syncher.remote_base_path, suite.name)))
     syncher.fetch(rel_filenames)
+    for filename in args.filenames:
+        logger.info('Fetched {} from "{}"'.format(filename, syncher.remote_name))
