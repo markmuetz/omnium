1dd1ffa3dc73f40ff9641ebadb20635e261cf5ab
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-12-12 15:18:10 +0000

    At least one arg is required for calling `omnium` from command line now.

--- omnium/command_parser.py
+++ omnium/command_parser.py
@@ -11,6 +11,7 @@ def parse_commands(name, top_level_args, module, cmdline_args):
     # create the top-level parser.
     parser = argparse.ArgumentParser(prog=name)
     subparsers = parser.add_subparsers(dest='cmd_name')
+    subparsers.required = True
 
     for top_level_pos_args, top_level_kwargs in top_level_args:
         parser.add_argument(*top_level_pos_args, **top_level_kwargs)
