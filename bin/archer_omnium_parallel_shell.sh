7f8686f5ee3d49f85968139dd2f5c8bd2b16b65e
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-01-11 09:50:58 +0000

    Add to scripts for running on ARCHER without stepping on rose/cylc's toes.
    rose and cylc are Python2 only. This causes problems for me when running with Py3 omnium.
    The solution is to add an extra layer of indirection - instead of setting up Py3 (with iris)
    then calling omnium directly from cylc or rose, call a bash script which sets up Py3 and then
    launches omnium. This effectively shields the calling PBS script from the `module load`s performed.
    The two scripts added are the scripts that get called. In the future, these should be made more
    generic. Either omnium/bin should be added to PATH, or these should be copied to PATH (e.g.
    <cylc_suite>/bin).

---
+++ bin/archer_omnium_parallel_shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo $@
+
+source $WORK/load_p3c_iris.sh
+
+export HOME=$WORK
+export PATH=$PATH:$WORK/.local/bin/:$WORK/bin
+export CYLC_CONTROL=True
+export OMNIUM_BASE_SUITE_DIR=/work/n02/n02/mmuetz/cylc-run/
+export OMNIUM_ANALYSER_PKGS=scaffold
+
+aprun -n 10 -N 10 omnium $@
---
+++ bin/archer_omnium_serial_shell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo $@
+
+source $WORK/.anaconda3_setup.sh 
+source activate $WORK/conda/envs/iris36
+
+export HOME=$WORK
+export PATH=$PATH:$WORK/.local/bin/:$WORK/bin
+export CYLC_CONTROL=True
+export OMNIUM_BASE_SUITE_DIR=/work/n02/n02/mmuetz/cylc-run/
+export OMNIUM_ANALYSER_PKGS=scaffold
+
+omnium $@
