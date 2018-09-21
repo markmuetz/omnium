59ccc1d52aa43215745b8904da6bb727ec7a9518
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-09-21 14:36:33 +0100

    Add some nicer logging to analysis-setup.

--- omnium/cmds/analysis_setup.py
+++ omnium/cmds/analysis_setup.py
@@ -18,23 +18,29 @@ def main(cmd_ctx, args):
     analysis_pkg_names = analysis_pkgs.split(':')
 
     for analysis_pkg_name in analysis_pkg_names:
+        logger.info('Setting up analysis_pkg: {}', analysis_pkg_name)
         analysis_pkg_config = cmd_ctx.suite.app_config['analysis_{}'.format(analysis_pkg_name)]
         repo = analysis_pkg_config['repo']
         reponame = analysis_pkg_config['reponame']
         commit = analysis_pkg_config['commit']
-
+        logger.info('  analysis_pkg repo: {}', repo)
+        logger.info('  analysis_pkg reponame: {}', reponame)
+        logger.info('  analysis_pkg commit: {}', reponame)
         cwd = os.getcwd()
 
         if os.path.exists(reponame):
+            logger.debug('repo exists: pulling')
             os.chdir(reponame)
             cmd = 'git pull'
             sp.call(cmd, shell=True)
         else:
+            logger.debug('repo does not exist: cloning')
             cmd = 'git clone {} {}'.format(repo, reponame)
             sp.call(cmd, shell=True)
             os.chdir(reponame)
 
         cmd = 'git checkout {}'.format(commit)
+        logger.debug('checking out {}', commit)
         sp.call(cmd, shell=True)
 
         os.chdir(cwd)
