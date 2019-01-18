06a4c6210147b9168c21405159ebac664635159c
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-01-18 10:26:38 +0000

    Fix #72: Allow for local packages.
    Throw an error if attempting to use a local git repo without the local
    setting. Done by symlinking to directory in
    <suite>/.omnium/analysis_pkgs/

--- omnium/cmds/analysis_setup.py
+++ omnium/cmds/analysis_setup.py
@@ -3,6 +3,8 @@ import os
 import subprocess as sp
 from logging import getLogger
 
+from omnium.omnium_errors import OmniumError
+
 logger = getLogger('om.analysis_setup')
 
 ARGS = []
@@ -20,9 +22,31 @@ def main(cmd_ctx, args):
     for analysis_pkg_name in analysis_pkg_names:
         logger.info('Setting up analysis_pkg: {}', analysis_pkg_name)
         analysis_pkg_config = cmd_ctx.suite.app_config['analysis_{}'.format(analysis_pkg_name)]
+
+        local = analysis_pkg_config.getboolean('local', False)
         repo = analysis_pkg_config['repo']
         reponame = analysis_pkg_config['reponame']
         commit = analysis_pkg_config['commit']
+
+        if local:
+            logger.info('Using local package')
+            assert os.path.exists(repo)
+
+            if os.path.exists(reponame):
+                if os.path.islink(reponame):
+                    logger.debug('local symlink already exists')
+                else:
+                    raise OmniumError('Reponame {} already exists in {} and is not a symlink\n'
+                                      'Try removing directory manually'
+                                      .format(reponame, os.getcwd()))
+            else:
+                logger.info('Creating symlink {}', reponame)
+                os.symlink(repo, reponame)
+            continue
+
+        if os.path.exists(repo):
+            raise OmniumError('repo {} is a local path: use the local=True setting'.format(repo))
+
         logger.info('  analysis_pkg repo: {}', repo)
         logger.info('  analysis_pkg reponame: {}', reponame)
         logger.info('  analysis_pkg commit: {}', commit)
