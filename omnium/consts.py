deb0e96ef4cd44f473856db004afc04432fae6cf
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-01-22 11:32:00 +0000

    Update consts with some more values and where they came from.

--- omnium/consts.py
+++ omnium/consts.py
@@ -1,4 +1,20 @@
+# See um/src/constants/planet_constants.F90
+# See um/src/constants/water_constants_mod.F90
+
+# earth_radius
 Re = 6371229.
+# earth_g
+g = 9.80665
+
+# earth_r
+Rd = 287.05
+# Lc
 L = 2.501e6
-cp = 1004
-g = 9.81
+# earth_cp
+cp = 1005
+
+# earth_pref
+p_ref = 100000
+
+kappa = Rd / cp
+
